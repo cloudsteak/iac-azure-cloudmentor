9e4db74c190c4d504796c9b5e4eb17fca0fc4196
the1bit <tibor.kiss@cloudmentor.hu>
2025-04-03 08:57:58 +0200

    Helm preparation

--- cloudmentor/data.tf
+++ cloudmentor/data.tf
@@ -1,1 +1,7 @@
 data "azurerm_client_config" "current" {}
+
+
+data "azurerm_key_vault_secret" "kube_config" {
+  name         = "${var.aks_name}-kube-config"
+  key_vault_id = module.KEY_VAULT.key_vault_id
+}
